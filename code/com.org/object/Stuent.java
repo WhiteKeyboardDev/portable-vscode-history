2c7bb1eeeb7985cee644ecfe3a6b686e795f7373
bfh3000 <bfh3000@naver.com>
2022-02-17 11:06:23 +0900

    ArrayList 중복 제거

---
+++ code/com.org/object/Stuent.java
@@ -0,0 +1,19 @@
+package object;
+
+public class Stuent {
+	String name = "";
+	String num = "";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+}
