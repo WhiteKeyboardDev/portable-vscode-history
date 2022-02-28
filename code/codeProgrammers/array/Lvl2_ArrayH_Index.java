736c0cf3978a055921429edff49b81f70d0060b1
bfh3000 <bfh3000@naver.com>
2022-02-28 16:23:29 +0900

    update 3nd

---
+++ code/codeProgrammers/array/Lvl2_ArrayH_Index.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package array;
+
+/**
+ * @author df321
+ *
+ */
+public class Lvl2_ArrayH_Index {
+
+	/**
+	 * https://programmers.co.kr/learn/courses/30/lessons/42747
+	 * 아니 문제가 무슨 문제냐...? 이해가 전혀 안가네
+	 */
+	
+	public int solution(int[] citations) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    } 
+	
+	public static void main(String[] args) {
+
+	}
+
+}
