d0ea4b30fb369babce3f5b8cefd148db4267c8fe
bfh3000 <bfh3000@naver.com>
2022-03-14 14:02:00 +0900

    memo update

---
+++ code/main/express/Memo.java
@@ -0,0 +1,18 @@
+package express;
+
+public class Memo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyArray<T>
+
+	}
+	
+	class MyArray<T> {
+	    T element;
+	    void setElement(T element) { this.element = element; }
+	    T getElement() { return element; }
+	}
+
+}
