da486177918723bd39acba6c01c68f2560daca8a
bfh3000 <bfh3000@naver.com>
2022-03-18 08:49:01 +0900

    memo T update

--- code/main/express/Memo.java
+++ code/main/express/Memo.java
@@ -5,11 +5,14 @@ public class Memo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
-	
 	class MyArray<T> {
 	    T element;
 	    void setElement(T element) { this.element = element; }
 	    T getElement() { return element; }
+	}
+	
+	class Ma<T>{
+		T structrue;
 	}
 
 }
