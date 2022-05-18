9f5277beb24c43a010aa9c6d4ff664d2b2f9524f
bfh3000 <bfh3000@naver.com>
2022-05-18 10:01:12 +0900

    update programmers lv1

--- code/main/tmp/sizeSquare.java
+++ code/main/tmp/sizeSquare.java
@@ -1,5 +1,4 @@
-package code.main.tmp;
-
+package tmp;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,20 +8,30 @@ public class sizeSquare {
 
 
     public static void main(String[] args) {
-        long tmp = Solution.solution(2, 20, 5);
+        long tmp = mySolution.solution(3, 20, 4);
         log.debug(tmp);
     }
 
+    //my1
+    static class mySolution{
+    	public static long solution(int price, int money, int count) {
+    		long answer = -1;
+    		answer++;
+    		for (int i = 1; i <= count; i++) {
+				answer += i*price;
+			}
+    		
+    		return (answer > money ?  answer - money : 0);
+    	}
+    }
+    
+    //doc.
     static class Solution {
-
         public static long solution(int price, int money, int count) {
-
             long answer = money;
-
             for (int cnt = 0; cnt < count; ++cnt) {
                 answer -= (price * (cnt + 1));
             }
-
             return (answer > 0 ? 0 : -answer);
         }
     }
