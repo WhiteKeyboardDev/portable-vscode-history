da48ff563ecff19d1124942ab7a8f46c426aa288
bfh3000 <bfh3000@naver.com>
2022-05-19 11:43:56 +0900

    junit5 test

--- code/main/express/Memo.java
+++ code/main/express/Memo.java
@@ -14,5 +14,4 @@ public class Memo {
 	class Ma<T>{
 		T structrue;
 	}
-
 }
--- code/main/idchoose/IdChoose.java
+++
@@ -1,8 +0,0 @@
-package idchoose;
-
-public class IdChoose {
-	 public String solution(String new_id) {
-        String answer = "";
-        return answer;
-    }
-}
